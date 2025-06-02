48d5dcd0313aeeea50921f883af28109d1a81024
Aldiyar Kakimov <aldikk@Aldiyars-MacBook-Air.local>
2025-06-02 19:26:33 +0500

    MAJOR FIX

--- src/main/java/ru/rayyxd/aetpreparation/AETApplication.java
+++ src/main/java/ru/rayyxd/aetpreparation/AETApplication.java
@@ -2,8 +2,6 @@ package ru.rayyxd.aetpreparation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
-import time;
-
 
 @SpringBootApplication
 @EnableTransactionManagement
